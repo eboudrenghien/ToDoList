d46308c37829d74d4a7d40a551aae760f04fc5d1
eboudrenghien <e.boudrenghien@codeur.online>
2022-03-08 17:36:27 +0100

    Ajout de la fonction champ requis, erreur : tâche accomplie une fois sur deux. Solution à trouver.

--- app.js
+++ app.js
@@ -17,82 +17,73 @@ function addTodo(event) {
     // création d'une liste non ordonnée
     const newTodo = document.createElement("li");
     newTodo.innerText = todoText.value;
-    //texte pour tester si ça marche
+
+  verification(document.formulaire.champ.value)
+  function verification(champ) {
+    if (champ =="") {
+      alert ("Veuillez saisir une tâche, jeune padawan");
+      return false;
+    } else {
  
     // ajoute une class todo-item
-    newTodo.classList.add("todo-item");
+      newTodo.classList.add("todo-item");
     //permet de créer un enfant (newTodo, liste) à la div todoDiv
-    todoDiv.appendChild(newTodo);
+      todoDiv.appendChild(newTodo);
     // saveLocalTaches(todoText.value);
-    todoList.appendChild(todoDiv);
-    todoText.value = ""; 
+      todoList.appendChild(todoDiv);
+      todoText.value = "";
 
+
+  
     // création du bouton check
-    const checkButton = document.createElement("button");
-    checkButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-    checkButton.classList.add("check-button");
-    todoDiv.appendChild(checkButton);
+      const checkButton = document.createElement("button");
+      checkButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+      checkButton.classList.add("check-button");
+      todoDiv.appendChild(checkButton);
 
-    // création du bouton delete;
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-    deleteButton.classList.add("delete-button");
-    todoDiv.appendChild(deleteButton);
+      // création du bouton delete;
+      const deleteButton = document.createElement("button");
+      deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+      deleteButton.classList.add("delete-button");
+      todoDiv.appendChild(deleteButton);
 
-    todoList.addEventListener("click",supprimerCheck);
-    function supprimerCheck(e){
-      const tache = e.target;   
-      if(tache.classList[0] === "delete-button"){
-        const todo = tache.parentElement;
-        removeLocaleTaches(todo);
-      todo.remove();
+      todoList.addEventListener("click",supprimerCheck);
+      function supprimerCheck(e){
+        const tache = e.target;   
+        if (tache.classList[0] === "delete-button") {
+          const todo = tache.parentElement;
+          removeLocaleTaches(todo);
+          todo.remove();
+        }
+        if (tache.classList[0] === "check-button") {
+          const todo = tache.parentElement;    
+          todo.classList.toggle("completed");
+        }
       }
-      if(tache.classList[0] === "check-button"){
-        const todo = tache.parentElement;    
-        todo.classList.toggle("completed");
-      }
-    }
 
-    // création du bouton editer; 
+      // création du bouton editer; 
       const editButton = document.createElement("button")
       editButton.innerHTML='<i class="fa-solid fa-ellipsis"></i>';
       editButton.classList.add("edit-button");
       todoDiv.appendChild(editButton);
       editButton.addEventListener("click", editerTaches);
 
-      
-    function editerTaches (e){
-      const tache = e.target;
-      if(tache.classList[0] === "edit-button"){
-      // const todo = tache.parentElement 
-      const input = document.createElement("input")
-      newTodo.innerText = input.value;
-      newTodo.appendChild(input);
-       //Création de la liste de tâche
-    // ajouter notre tâche à la liste todo
+      function editerTaches(e) {
+        const tache = e.target;
+        if (tache.classList[0] === "edit-button") {
+        // const todo = tache.parentElement 
+          const input = document.createElement("input")
+          newTodo.innerText = input.value;
+          newTodo.appendChild(input);
+        }
+      }
+    }
+  } 
 }
-
-    // création bouton sauvegarde + ajout événement
-
-    // const sauvegarderButton = document.createElement("button")
-    // sauvegarderButton.innerHTML = '<i class="fa-solid fa-floppy-disk"></i>';
-    // sauvegarderButton.classList.add("sauvegarder-button");
-    // editButton.appendChild(sauvegarderButton)
-    // });
-    //création bouton annuler + ajout événement
-    // const annulerButton = document.createElement("button")
-    // annulerButton.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-    // annulerButton.classList.add("annuler-button"),
-    // newTodo.appendChild(annulerButton);
-    // annulerButton.addEventListener("click", annulerTaches);
-
-  }
-}
-
 // Filtre
-function filtreTodo(e){
+function filtreTodo(e) {
   const taches = todoList.childNodes;
-  taches.forEach(function(todo){
+  taches.forEach(function(todo) {
     switch (e.target.value){
       case "toutes":
         todo.style.display = "flex";
@@ -100,19 +91,19 @@ function filtreTodo(e){
       case "termine":
         if (todo.classList.contains("completed")){
           todo.style.display = "flex";
-    } else {
-      todo.style.display = "none";
-    }
+        } else {
+          todo.style.display = "none";
+        }
       break;
-    case "encours":
-      if (!todo.classList.contains("completed")){
-        todo.style.display = "flex";
-      } else {
-        todo.style.display = "none";
-      }
+      case "encours":
+        if (!todo.classList.contains("completed")){
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
       break;
     }
-  });
+  })
 }
 
 // Sauvegarde
@@ -128,14 +119,14 @@ function filtreTodo(e){
 //   localStorage.setItem("taches", JSON.stringify(taches));
 // }
 
-function getTaches(){
+function getTaches() {
   let taches;
-  if (localStorage.getItem("taches") === null){
-    taches = [];
-  } else{
-    taches = JSON.parse(localStorage.getItem("taches"));
-  }
-  taches.forEach(function(todo){
+    if (localStorage.getItem("taches") === null) {
+      taches = [];
+    } else {
+      taches = JSON.parse(localStorage.getItem("taches"));
+    }
+    taches.forEach(function(todo){
     const todoDiv = document.createElement("div"); 
     todoDiv.classList.add("todo");
     // création d'une liste non ordonnée
@@ -156,21 +147,21 @@ function getTaches(){
     todoDiv.appendChild(deleteButton);
 
       // création du bouton editer; 
-      const editButton = document.createElement("button");
-      editButton.innerHTML='<i class="fa-solid fa-ellipsis"></i>';
-      editButton.classList.add("edit-button");
-      todoDiv.appendChild(editButton);
+    const editButton = document.createElement("button");
+    editButton.innerHTML='<i class="fa-solid fa-ellipsis"></i>';
+    editButton.classList.add("edit-button");
+    todoDiv.appendChild(editButton);
     todoList.appendChild(todoDiv);
-  })
+    })
 }
 
-function removeLocaleTaches(todo){
+function removeLocaleTaches(todo) {
   let taches;
-  if (localStorage.getItem("taches") === null){
-    taches = [];
-  } else{
-    taches = JSON.parse(localStorage.getItem("taches"));
-  }
+    if (localStorage.getItem("taches") === null) {
+      taches = [];
+    } else {
+      taches = JSON.parse(localStorage.getItem("taches"));
+    }
   const todoIndex = todo.children[0].innerText;
   taches.splice(taches.indexOf(todoIndex),1);
   localStorage.setItem("taches", JSON.stringify(taches));
